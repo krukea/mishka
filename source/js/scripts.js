b87d65be522e7b52d938c65cfdb90f286398c948
krukea <kruk-ea@itechlab.net>
2023-09-06 16:19:02 +0300

    feat: adaptive popup styles + open popup js

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -5,3 +5,23 @@ menyToggle.addEventListener("click", (e) => {
   mainNav.classList.toggle("main-nav--closed");
   mainNav.classList.toggle("main-nav--opened");
 });
+
+const openPopup = document.querySelector(".buy__btn");
+const popup = document.querySelector(".add-to-cart");
+
+openPopup.addEventListener("click", (e) => {
+  popup.classList.remove("modal--hidden");
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    popup.classList.add("modal--hidden");
+  }
+});
+
+popup.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target === popup && !popup.classList.contains("modal--hidden")) {
+    popup.classList.add("modal--hidden");
+  }
+});
