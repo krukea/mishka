b75eedee587c693f452c2f51d00ab187f5e889c9
krukea <kruk-ea@itechlab.net>
2023-09-12 17:14:54 +0300

    feat: добавить стили меню при отсутствии js

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,6 +1,8 @@
 const menyToggle = document.querySelector(".main-nav__toggle");
 const mainNav = document.querySelector(".main-nav");
 
+mainNav.classList.remove("main-nav--no-js");
+
 menyToggle.addEventListener("click", (e) => {
   mainNav.classList.toggle("main-nav--closed");
   mainNav.classList.toggle("main-nav--opened");
